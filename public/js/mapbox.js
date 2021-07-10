aa693e10607ef7e661b6c2813548592b4229f0a5
lilanika <68299182+lilanika@users.noreply.github.com>
2021-07-10 09:41:49 +0200

    added logo, secondary color, secondary btn and revised header

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,11 +1,14 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoib21nbiIsImEiOiJja2w1Z2FkMGoxa3phMm5vNG5sY2ZmbmJoIn0.Bbu_dXdzyIDaG9jUy6LYhw'; 
 
+
+
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/omgn/cklbf24ba0c9i17t551ed3mgb', // style URL
   center: [13.405, 52.52], // starting position [lng, lat]
   zoom: 12 // starting zoom
 });
+
 
 const nav = new mapboxgl.NavigationControl(); 
 map.addControl(nav, 'bottom-right'); 
