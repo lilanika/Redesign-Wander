6efc53d17c36dd2fb1d5b81cc18b3b43281270e8
lila <vivalafabulous@gmail.com>
2022-10-13 21:48:24 +0200

    style:change some stylings

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,26 +1,18 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoib21nbiIsImEiOiJja2w1Z2FkMGoxa3phMm5vNG5sY2ZmbmJoIn0.Bbu_dXdzyIDaG9jUy6LYhw";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoib21nbiIsImEiOiJja2w1Z2FkMGoxa3phMm5vNG5sY2ZmbmJoIn0.Bbu_dXdzyIDaG9jUy6LYhw'; 
-/*mapboxgl.accessToken = 'pk.eyJ1IjoibGlsYXdvbGtlIiwiYSI6ImNrbDZsejRzdDFleHQyd21uejVpNmFjd2kifQ.HKVGmke-kcUCQhJw6M_BgQ'; */
-
-
-
+  // mapboxgl.accessToken = 'pk.eyJ1IjoibGlsYXdvbGtlIiwiYSI6ImNrbDZsejRzdDFleHQyd21uejVpNmFjd2kifQ.HKVGmke-kcUCQhJw6M_BgQ'; 
 
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/omgn/cklbf24ba0c9i17t551ed3mgb', // style URL*/
- /*style: 'mapbox://styles/mapbox/light-v10',  */
+  container: "map", // container ID
+  style: "mapbox://styles/omgn/cklbf24ba0c9i17t551ed3mgb", // style URL*/
+  /*style: 'mapbox://styles/mapbox/light-v10',  */
   center: [13.405, 52.52], // starting position [lng, lat] */
-  zoom: 12 // starting zoom
+  zoom: 12, // starting zoom
 });
 
- 
-
-
-
-
-
-const nav = new mapboxgl.NavigationControl(); 
-map.addControl(nav, 'bottom-right'); 
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, "bottom-right");
 
 // Marker
 var marker = new mapboxgl.Marker() // initialize a new marker
@@ -28,168 +20,154 @@ var marker = new mapboxgl.Marker() // initialize a new marker
   .addTo(map); // Add the marker to the map
 
 // Searchbar
-  var geocoder = new MapboxGeocoder({ // Initialize the geocoder
-    accessToken: mapboxgl.accessToken, // Set the access token
-    mapboxgl: mapboxgl, // Set the mapbox-gl instance
-    marker: false, // Do not use the default marker style
-    placeholder: 'Search for places'
-  });
-  
-  // Add the geocoder to the map
-  /*map.addControl(geocoder); */
-  
 var geocoder = new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken,
-  mapboxgl: mapboxgl
+  // Initialize the geocoder
+  accessToken: mapboxgl.accessToken, // Set the access token
+  mapboxgl: mapboxgl, // Set the mapbox-gl instance
+  marker: false, // Do not use the default marker style
+  placeholder: "Search for places",
 });
 
-document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+// Add the geocoder to the map
+/*map.addControl(geocoder); */
 
- 
+var geocoder = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  mapboxgl: mapboxgl,
+});
 
-  
-  //marker 
+document.getElementById("geocoder").appendChild(geocoder.onAdd(map));
 
-  // After the map style has loaded on the page,
+//marker
+
+// After the map style has loaded on the page,
 // add a source layer and default styling for a single point
-map.on('load', function() {
-  map.addSource('single-point', {
-    type: 'geojson',
+map.on("load", function () {
+  map.addSource("single-point", {
+    type: "geojson",
     data: {
-      type: 'FeatureCollection',
-      features: []
-    }
+      type: "FeatureCollection",
+      features: [],
+    },
   });
 
   map.addLayer({
-    id: 'point',
-    source: 'single-point',
-    type: 'circle',
+    id: "point",
+    source: "single-point",
+    type: "circle",
     paint: {
-      'circle-radius': 10,
-      'circle-color': '#448ee4'
-    }
+      "circle-radius": 10,
+      "circle-color": "#448ee4",
+    },
   });
 
   // Listen for the `result` event from the Geocoder
   // `result` event is triggered when a user makes a selection
   //  Add a marker at the result's coordinates
-  geocoder.on('result', function(e) {
-    map.getSource('single-point').setData(e.result.geometry);
+  geocoder.on("result", function (e) {
+    map.getSource("single-point").setData(e.result.geometry);
     console.log(e.result);
 
- 
-
     const long = e.result.geometry.coordinates[0];
-    const lat = e.result.geometry.coordinates[1]; 
-    const name = e.result.text; 
-    const address = e.result.place_name; 
-    const category = e.result.properties.category; 
-    document.querySelector('#saveLocation').onclick = function (){
-      console.log(long, lat) 
-     // axios.post('http://localhost:3000/addPlace', {coordinates: [long, lat], name: name, address: address, category: category} )
+    const lat = e.result.geometry.coordinates[1];
+    const name = e.result.text;
+    const address = e.result.place_name;
+    const category = e.result.properties.category;
+    document.querySelector("#saveLocation").onclick = function () {
+      console.log(long, lat);
+      // axios.post('http://localhost:3000/addPlace', {coordinates: [long, lat], name: name, address: address, category: category} )
 
-    
-        axios.post('https://wander-share-your-spots.herokuapp.com/addPlace', {coordinates: [long, lat], name: name, address: address, category: category} )
-      .then(response => {
-        showMarkers(); 
-        showPopUp(); 
-      })
+      axios
+        .post("https://wander-share-your-spots.herokuapp.com/addPlace", {
+          coordinates: [long, lat],
+          name: name,
+          address: address,
+          category: category,
+        })
+        .then((response) => {
+          showMarkers();
+          showPopUp();
+        })
 
-      .catch(err => console.log(err))
-    }
+        .catch((err) => console.log(err));
+    };
 
-    // //Update the marker 
-     document.querySelector('#saveTag').onclick = function (){
-
-    }
-
-
+    // //Update the marker
+    document.querySelector("#saveTag").onclick = function () {};
   });
 });
 
-
-
-
-function showMarkers(){
-  console.log('SHOW MARKERS'); 
+function showMarkers() {
+  console.log("SHOW MARKERS");
   //axios.get('http://localhost:3000/api/favoritesPlaces').then(response =>{
- 
-  axios.get('https://wander-share-your-spots.herokuapp.com/api/favoritesPlaces').then(response =>{
-  const places = response.data.data; 
-  let color = 'red'; 
-  console.log(places); 
-  places.forEach(place => {
-    if (place.tag === 'Food') {
-      color = '#9b5de5'
-    } else if (place.tag === 'Bar'){
-      color = '#c65ccd'
-    } else if (place.tag === 'Park'){
-      color = '#f15bb5'
-    } else if (place.tag === 'Sports'){
-      color = '#fee440'
-    } else if (place.tag === 'Culture'){
-      color = 'peachpuff'
-    } else if (place.tag === 'Club'){
-      color = '#5d9e87'
-    } else if (place.tag === 'overnight'){
-      color = '#00C8AA'
-    } else {
-      color = 'amarine'
-    }
-    new mapboxgl.Marker({
-      scale: 1,
-      color: color,
-  })
-      .setLngLat(place.coordinates)
-      .addTo(map)
-      .on('dragend', (data) => {
-          console.log(data);
-      })
 
-  })
-})
+  axios
+    .get("https://wander-share-your-spots.herokuapp.com/api/favoritesPlaces")
+    .then((response) => {
+      const places = response.data.data;
+      let color = "red";
+      console.log(places);
+      places.forEach((place) => {
+        if (place.tag === "Food") {
+          color = "#9b5de5";
+        } else if (place.tag === "Bar") {
+          color = "#c65ccd";
+        } else if (place.tag === "Park") {
+          color = "#f15bb5";
+        } else if (place.tag === "Sports") {
+          color = "#fee440";
+        } else if (place.tag === "Culture") {
+          color = "peachpuff";
+        } else if (place.tag === "Club") {
+          color = "#5d9e87";
+        } else if (place.tag === "overnight") {
+          color = "#00C8AA";
+        } else {
+          color = "amarine";
+        }
+        new mapboxgl.Marker({
+          scale: 1,
+          color: color,
+        })
+          .setLngLat(place.coordinates)
+          .addTo(map)
+          .on("dragend", (data) => {
+            console.log(data);
+          });
+      });
+    });
 }
 
-showMarkers(); 
+showMarkers();
 
-function showPopUp(){
+function showPopUp() {
+  //axios.get('http://localhost:3000/api/favoritesPlaces').then(response => {
 
-   //axios.get('http://localhost:3000/api/favoritesPlaces').then(response => {
-      
-    axios.get('https://wander-share-your-spots.herokuapp.com/api/favoritesPlaces').then(response => {
-    
-    const places = response.data.data; 
-    places.forEach(place => {
-      new mapboxgl.Popup({
-        closeButton: true
-      }) 
-               .setLngLat(place.coordinates)
-               .setHTML(place.name)
-               .setMaxWidth('200px')
-               .addTo(map);
-    })
-  })
+  axios
+    .get("https://wander-share-your-spots.herokuapp.com/api/favoritesPlaces")
+    .then((response) => {
+      const places = response.data.data;
+      places.forEach((place) => {
+        new mapboxgl.Popup({
+          closeButton: true,
+        })
+          .setLngLat(place.coordinates)
+          .setHTML(place.name)
+          .setMaxWidth("200px")
+          .addTo(map);
+      });
+    });
 }
 
-
-
-
-showPopUp(); 
-
-
-
-
+showPopUp();
 
 // color: #00b7ff;
 //  background: #122933;
 //   background: #8eb2c4;
 
-
-
 // function showPopUp (){
 //   axios.get('http://localhost:3000/api/favoritesPlaces').then(response =>{
-//   const places = response.data.data; 
+//   const places = response.data.data;
 //   places.forEach(place => {
 //     new mapboxgl.Popup({
 //          closeButton: true
@@ -212,21 +190,13 @@ showPopUp();
 //   .setMaxWidth('400px')
 //   .addTo(map)
 
-
-
-
-
-
-
 // color: #00b7ff;
 //  background: #122933;
 //   background: #8eb2c4;
 
-
-
 // function showPopUp (){
 //   axios.get('http://localhost:3000/api/favoritesPlaces').then(response =>{
-//   const places = response.data.data; 
+//   const places = response.data.data;
 //   places.forEach(place => {
 //     new mapboxgl.Popup({
 //          closeButton: true
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const User = require('../models/User.model');
-const bcrypt = require('bcrypt');
+const User = require("../models/User.model");
+const bcrypt = require("bcrypt");
 
 // signup
 router.get("/signup", (req, res, next) => {
@@ -12,77 +12,77 @@ router.get("/login", (req, res, next) => {
   res.render("login");
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   const { username, password } = req.body;
   // check if we have a user with the entered username
-  User.findOne({ username: username })
-    .then(userFromDB => {
-      if (userFromDB === null) {
-        // if not we show login again
-        res.render('login', { message: 'Invalid credentials' });
-        return;
-      }
-      // if username is existing then we want to check the password
-      if (bcrypt.compareSync(password, userFromDB.password)) {
-        // password and hash match
-        // now we want to log the user in
-        req.session.user = userFromDB;
-        res.redirect('/homepage');
-      } else {
-        res.render('login', { message: 'Invalid credentials' });
-      }
-    })
-
-})
+  User.findOne({ username: username }).then((userFromDB) => {
+    if (userFromDB === null) {
+      // if not we show login again
+      res.render("login", { message: "Invalid credentials" });
+      return;
+    }
+    // if username is existing then we want to check the password
+    if (bcrypt.compareSync(password, userFromDB.password)) {
+      // password and hash match
+      // now we want to log the user in
+      req.session.user = userFromDB;
+      res.redirect("/homepage");
+    } else {
+      res.render("login", { message: "Invalid credentials" });
+    }
+  });
+});
 
 // the signup form posts to this route
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   // get username and password
   const { username, password } = req.body;
   console.log(username, password);
   // is the password longer than 8 chars and the username not empty
   if (password.length < 8) {
     // if not show the signup again with a message
-    return res.render('signup', { message: 'Your password has to be 8 chars min' });
-
+    return res.render("signup", {
+      message: "Your password has to be 8 chars min",
+    });
   }
-  if (username === '') {
-    res.render('signup', { message: 'Your username cannot be empty' });
-    return
+  if (username === "") {
+    res.render("signup", { message: "Your username cannot be empty" });
+    return;
   }
   // check if the username already exists
   User.findOne({ username: username })
-    .then(userFromDB => {
+    .then((userFromDB) => {
       if (userFromDB !== null) {
         // if yes show the signup again with a message
-        res.render('signup', { message: 'Username is already taken' });
+        res.render("signup", { message: "Username is already taken" });
       } else {
         // all validation passed - > we can create a new user in the database with a hashed password
         // create salt and hash
         const salt = bcrypt.genSaltSync();
-        const hash = bcrypt.hashSync(password, salt)
+        const hash = bcrypt.hashSync(password, salt);
         // create the user in the db
-        User.create({ username: username, password: hash })
-          .then(userFromDB => {
+        User.create({ username: username, password: hash }).then(
+          (userFromDB) => {
             console.log(userFromDB);
             // then redirect to login
-            res.redirect('/');
-          })
+            res.redirect("/");
+          }
+        );
       }
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-    })
-})
+    });
+});
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.session.destroy(function (err) {
     if (err) {
       console.log(err);
     } else {
-      res.redirect('/');
+      res.redirect("/");
     }
-  })
-})
+  });
+});
 
 module.exports = router;
